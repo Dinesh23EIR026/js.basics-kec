e248c67c87979e16cbb5f8f85ba8e18493b50c16
Dinesh23EIR026 <dineshsrinivasan0308@gmail.com>
2025-06-19 11:20:19 +0530

    conditinal statement

---
+++ conditionals.js
@@ -0,0 +1,15 @@
+let marks = 95;
+
+if(marks >= 90){
+console.log("A grade");
+}else if(marks >= 75){
+    console.log("B grade");
+}else{
+    console.log("C Grade");
+}
+
+if(90){
+    console.log("True value");
+}else{
+    console.log("False value");
+}
