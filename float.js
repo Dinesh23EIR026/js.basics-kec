bd0f771a564203e26bbe0536a31dcf589a8ff0ca
Dinesh23EIR026 <dineshsrinivasan0308@gmail.com>
2025-06-19 10:56:21 +0530

    complete logical operators

---
+++ float.js
@@ -0,0 +1,12 @@
+// let a= 3.3;
+// let b=3.1;
+// console.log(a+b)
+
+// let result = ((3+9) / 10) . toFixed(99);
+// console.log(result)
+
+
+console.log("a" > "b")
+console.log("2" == 2)
+console.log("2" !== 2)
+console.log("2" != 2)
